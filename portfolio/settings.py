35f786f90977bc0c1f8d0b4499f36cdec5b6c170
Ubuntu <ubuntu@ip-172-31-24-22.ec2.internal>
2022-10-20 18:39:29 +0000

    update settings.py and create new project object in DB

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -162,7 +162,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
