1a4d32f7d8bee203008f7793b09c36a975d07316
JVC <vancleave.jacob@gmail.com>
2022-12-07 12:25:42 -0500

    install postgresql

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -95,12 +95,22 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'jvc_design',
+        'USER': 'jacob',
+        'PASSWORD': 'janis',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
-
+}
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql',
     #     'NAME': 'test',
@@ -109,7 +119,7 @@ DATABASES = {
     #     'HOST': 'localhost',
     #     'PORT': '5432',
     # }
-}
+# }
 
 
 # Password validation
